a15d1ec14f5cd7a18ee7286c0aa6d94aa571a0fa
ellieisjelly <ellie@ellieis.me>
2026-05-16 22:28:04 -0300

    Fix console errors

--- src/main/java/io/github/haykam821/diceyheights/game/map/DiceyHeightsMapGenerator.java
+++ src/main/java/io/github/haykam821/diceyheights/game/map/DiceyHeightsMapGenerator.java
@@ -3,6 +3,7 @@ package io.github.haykam821.diceyheights.game.map;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.biome.Biomes;
@@ -19,16 +20,21 @@ public class DiceyHeightsMapGenerator extends GameChunkGenerator {
     public DiceyHeightsMapGenerator(DiceyHeightsMapConfig config, MinecraftServer server) {
         super(createBiomeSource(server, Biomes.THE_VOID));
         this.config = config;
-
     }
     @Override
     public void applyBiomeDecoration(WorldGenLevel level, ChunkAccess chunk, StructureManager structureAccessor) {
+        ChunkPos chunkPos = chunk.getPos();
         RandomSource random = level.getRandom();
         int waitingPlatformY = START_Y + config.waitingPlatformHeight().sample(random);
         BlockBounds waitingPlatformBounds = BlockBounds.of(-5, waitingPlatformY, -5, 5, waitingPlatformY, 5);
-        for (BlockPos pos : waitingPlatformBounds) {
-            BlockState state = this.config.waitingPlatformProvider().getState(level, random, pos);
-            level.setBlock(pos, state, 0, 0);
+        BlockPos min = new BlockPos(chunkPos.x() * 16, waitingPlatformY, chunkPos.z() * 16);
+        BlockPos max = new BlockPos(chunkPos.x() * 16 + 15, waitingPlatformY, chunkPos.z() * 16 + 15);
+        BlockBounds chunkBounds = new BlockBounds(min, max);
+        if (chunkBounds.intersects(waitingPlatformBounds)) {
+            for (BlockPos pos : waitingPlatformBounds) {
+                BlockState state = this.config.waitingPlatformProvider().getState(level, random, pos);
+                chunk.setBlockState(pos, state);
+            }
         }
     }
 }
